af6e126a885695155e6c4cea7b0747677918f0c1
Noel Vallejos <noel.72712510.va@gmail.com>
2024-05-16 11:35:38 -0400

    ......

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/sitio/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "proyecto";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+
+const USER_SMTP = "";
+const PASS_SMTP = "";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
