23340f7a6639496118fc4f216fa4a8876b5beae9
vallejos <vallejosnoe578@gmail.com>
2024-05-23 14:09:59 -0400

    agregando productos de prueba   :no funciona el correo

--- Config/Config.php
+++ Config/Config.php
@@ -5,12 +5,12 @@ const USER = "root";
 const PASS = "";
 const DB = "chaski";
 const CHARSET = "charset=utf8";
-const TITLE = "TIENDA ONLINE";
-const MONEDA = "USD";
-const CLIENT_ID = "";
+const TITLE = "TIENDA";
+const MONEDA = "Bs";
+const CLIENT_ID = "AVDywYweO-VjBRqIYXyPUh290DQh0zFos436R24tmcN699T4P1h4d0BxXDbNyGRbNjvxMOupbVAywSd_";
 
-const USER_SMTP = "";
-const PASS_SMTP = "";
+const USER_SMTP = "noel.72712510.va@gmail.com";
+const PASS_SMTP = "vkif djxq xchd pniy";
 const PUERTO_SMTP = 465;
 const HOST_SMTP = "smtp.gmail.com";
 ?>
